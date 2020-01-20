11e2963d47e9c536f3a683bc0fd155ac17558d9e
Gideon Paul <gidzpaul95@gmail.com>
2020-01-20 00:37:15 -0500

    Custom task states.

--- config/org.el
+++ config/org.el
@@ -1,22 +1,28 @@
 ;; Org mode config.
 
+;; Customizing task states.
+(setq org-todo-keywords '((sequence "TODO" "BLOCKED" "IN-PROGRESS" "DONE")))
+
+;; Org agenda.
+(setq org-agenda-files '("~/Crypt/notebook"))
+
 ;; Capture template config.
 (setq org-capture-templates
   '(    ;; ... other templates
 
     ("w" "Work Log Entry"
-         entry (file+datetree "~/Vault/notebook2/ncl.org")
+         entry (file+datetree "~/Crypt/notebook/ncl.org")
          "* %?"
          :empty-lines 0)
 
     ("t" "Todo Entry"
-         entry (file+datetree "~/Vault/notebook2/todo.org")
+         entry (file+datetree "~/Crypt/notebook/todo.org")
          " TODO %?"
          :empty-lines 0)
 
 
     ("f" "Fire Project Entry"
-         entry (file+datetree "~/Vault/notebook2/fireproject.org")
+         entry (file+datetree "~/Crypt/notebook/fireproject.org")
          "* %?"
          :empty-lines 0)
 
